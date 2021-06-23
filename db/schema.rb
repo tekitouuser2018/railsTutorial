ce6a2dbd61d86a6820b0d317f697f30f2a6e1e6a
tekitouuser2018 <booty6118@gmail.com>
2021-06-23 21:24:26 +0900

    ADD: acaffold 実行

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_23_084128) do
+ActiveRecord::Schema.define(version: 2021_06_23_120412) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,4 +22,14 @@ ActiveRecord::Schema.define(version: 2021_06_23_084128) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "weight_histories", force: :cascade do |t|
+    t.bigint "user_id"
+    t.integer "weight"
+    t.string "memo"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_weight_histories_on_user_id"
+  end
+
+  add_foreign_key "weight_histories", "users"
 end
