4363ec264962de06b45a500f2471f4ecc00b02ea
tekitouuser2018 <booty6118@gmail.com>
2021-06-23 20:59:42 +0900

    ADD: ユーザー一覧画面と各ユーザー画面から一覧へ戻るボタンを作成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,7 @@
 class UsersController < ActionController::Base
+    def index
+        @users = User.all
+    end
     def show
         @user = User.find(params["id"])
     end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'application#hello'
 
+  get '/users', to: 'users#index', as: "users"
+  # /users/1
   get '/users/:id', to: 'users#show', as: 'user'
 end
